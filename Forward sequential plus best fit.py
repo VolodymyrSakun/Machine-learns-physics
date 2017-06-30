2739c9498ab8eac1bc19aef1140247a88451edb9
VolodymyrSakun <volodymyr.sakun@mail.mcgill.ca>
2017-06-30 01:57:34 -0400

    Add files via upload

--- Forward sequential plus best fit.py
+++ Forward sequential plus best fit.py
@@ -1,14 +1,12 @@
 import numpy as np
 import pandas as pd
 from structure import library2
-import sklearn.metrics as skm
-from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import scale
 
 if __name__ == '__main__':
     MinCorr = 1e-7
-    NofFeatures = 20
+    NofFeatures = 15
     # Read features and structures from files stored by "Generate combined features"
     X, Y, FeaturesAll, FeaturesReduced = library2.ReadFeatures('Harmonic Features.csv', \
         'HarmonicFeaturesAll.dat', 'HarmonicFeaturesReduced.dat', verbose=False)
@@ -33,9 +31,9 @@ if __name__ == '__main__':
         nVariables = len(idx)+1
         idx_new = library2.ForwardSequential(x_std, y_std, nVariables=nVariables, idx=idx)
         idx_corr = library2.ClassifyCorrelatedFeatures(x_std, idx_new, MinCorrelation=MinCorr, Model=1, Corr_Matrix=C, verbose=False)    
-        idx_bestfit = library2.FindBestSetMP2(x_std, y_std, idx_new, idx_corr, Method='MSE', verbose=True)
+        idx_bestfit = library2.FindBestSet(x_std, y_std, idx_new, idx_corr, Method='MSE', verbose=True)
         idx = idx_bestfit
-        library2.Results_to_xls2(writeResults, str(len(idx)),\
+        library2.Results_to_xls3(writeResults, str(len(idx)),\
             idx, X_train, Y_train, X_test, Y_test, FeaturesAll, FeaturesReduced)
         nonzero, mse, rmse, r2, aIC = library2.get_fit_score(X_train, X_test, Y_train, Y_test, idx=idx)
         mse_list.append(mse)
@@ -46,46 +44,4 @@ if __name__ == '__main__':
 
     writeResults.save()
     library2.plot_rmse("Foprward Results", nonzero_list, rmse_list, r2_list)
-        
-"""    
-    nVariables=10 # Desired number of variables
-    idx=None # Initial fit if exists
-    lr = LinearRegression(fit_intercept=False, normalize=False, copy_X=True, n_jobs=-1)
-    if idx is None: # determine first feature
-        x_selected = np.zeros(shape=(Size, 1), dtype=float) # matrix with selected features
-        mse_array = np.zeros(shape=(NofFeatures), dtype=float)
-        for i in range(0, NofFeatures, 1):
-            x_selected[:, 0] = x_std[:, i]
-            lr.fit(x_selected, y_std)
-            y_pred = lr.predict(x_selected)
-            mse = skm.mean_squared_error(y_std, y_pred)
-            mse_array[i] = mse
-        idx1 = np.argmin(mse_array) # get most important feature
-        idx = [] #indices of selected features
-        idx.append(idx1)
-    x_selected = np.zeros(shape=(Size, len(idx)), dtype=float) # matrix with selected features
-    for i in range(0, len(idx), 1):
-        x_selected[:, i] = x_std[:, idx[i]]
-    z = np.zeros(shape=(Size, 1), dtype=float)
-    j = len(idx)
-    while j < nVariables:
-        print(j)
-        mse_array = np.ones(shape=(NofFeatures), dtype=float)
-        z = np.zeros(shape=(Size, 1), dtype=float)
-        x_selected = np.concatenate((x_selected, z), axis=1)
-        for i in range(0, NofFeatures, 1):
-            if i in idx:
-                continue
-            x_selected[:, -1] = x_std[:, i]
-            lr.fit(x_selected, y_std)
-            y_pred = lr.predict(x_selected)
-            mse = skm.mean_squared_error(y_std, y_pred)
-            mse_array[i] = mse
-        idx_mse_min = np.argmin(mse_array) # index of feature in current matrix with smallest RMSE
-        idx.append(idx_mse_min)
-        x_selected[:, -1] = x_std[:, idx_mse_min]
-        j += 1
-"""
-
-    
-        
+    print('DONE')
