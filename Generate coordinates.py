0aef715c8491a52ac9073523b16ae4388a84606e
VolodymyrSakun <volodymyr.sakun@mail.mcgill.ca>
2017-08-08 00:30:49 +0300

    Add files via upload

--- Generate coordinates.py
+++ Generate coordinates.py
@@ -1,11 +1,15 @@
 from structure import spherical
+import random
+from math import pi
+import numpy as np
+
 # read molecules from descriptor file
 Molecules = spherical.ReadMolecules(F='MoleculesDescriptor.')  
 # align molecules that its center of mass it in 0,0,0 and principal axis aligned along x, y, z
 Water = spherical.align_molecule(Molecules[0])
 Ethane = spherical.align_molecule(Molecules[1])
 # this is the example of using the function for your recursion
-A = spherical.f1(Ethane, 5, 5, 5, 45, 45, 45, CoordinateSystem='Cartesian', AngleType='Degree') 
+A = spherical.f1(Ethane, 5, 15, 25, 35, 45, 55, CoordinateSystem='Cartesian', AngleType='Degree') 
 # A is numpy array. it has 3 columns (x, y, z) and many rows that correspond to atom coordinates
 theta = 90 # angle between x axis and projection of point to xy plane
 phi = 90 # angle between z axis and O-P vector
@@ -31,7 +35,7 @@ ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
 # ax.set_aspect('equal')
-ax.auto_scale_xyz([0, 10], [0, 10], [0, 10])
+# ax.auto_scale_xyz([0, 6], [0, 6], [0, 6])
 # example of ethane rotation / translation
 spherical.plot_molecule(ax, Ethane)
 spherical.plot_molecule(ax, Ethane1, dot_color='black', bond_color='green')
@@ -39,7 +43,6 @@ spherical.plot_molecule(ax, Ethane2, dot_color='grey', bond_color='yellow')
 spherical.plot_molecule(ax, Ethane3, dot_color='pink', bond_color='magenta')
 # ax.plot([0, 5*rotation_axis.x], [0, 5*rotation_axis.y], [0, 5*rotation_axis.z])
 ax.legend()
-# plt.show()
 
 fig2 = plt.figure(101, figsize=(19,10))
 bx = fig2.gca(projection='3d')
@@ -47,20 +50,75 @@ bx.set_xlabel('X Label')
 bx.set_ylabel('Y Label')
 bx.set_zlabel('Z Label')
 # bx.set_aspect('equal')
-bx.auto_scale_xyz([0, 10], [0, 10], [0, 10])
+# bx.auto_scale_xyz([0, 10], [0, 10], [0, 10])
 # example of water rotation / translation
 spherical.plot_molecule(bx, Water)
 spherical.plot_molecule(bx, Water1, dot_color='black', bond_color='green')
 spherical.plot_molecule(bx, Water2, dot_color='grey', bond_color='yellow')
 spherical.plot_molecule(bx, Water3, dot_color='pink', bond_color='magenta')
 bx.legend()
-# bx.show()
 
+# generate coordinates
+RandomSeed = 101
+Radius = 10 # Radius of sphere where water molecules will be placed
+nMolecules = 4 # number of molecules in the system
+nRecords = 10000 # number of records
+# random.seed(RandomSeed)
+random.seed()
+Molecules = []
+Saturated = 0
+records = []
+for rec in range(0, nRecords, 1):
+    if rec % 100 == 0:
+        print(rec)
+    while (len(Molecules) < nMolecules) and (Saturated < 1000):
+        theta = 2*pi*random.random()
+        phi = 2*pi*random.random()
+        psi = 2*pi*random.random()
+        x = 2 * Radius * (random.random() - 0.5)
+        y = 2 * Radius * (random.random() - 0.5)
+        z = 2 * Radius * (random.random() - 0.5)
+        new_center = spherical.Point(x, y, z)
+        Water1 = spherical.rotate_molecule_angles(Water, theta, phi, psi, AngleType='Radian')
+        Water2 = spherical.translate_molecule_to_new_center(Water1, new_center)
+        Good = True
+        for j in Molecules:
+            for k in j.Atoms:
+                x1 = k.x
+                y1 = k.y
+                z1 = k.z
+                r1 = k.Atom.Radius
+                for l in Water2.Atoms:
+                    x2 = l.x
+                    y2 = l.y
+                    z2 = l.z
+                    r2 = l.Atom.Radius
+                    critical_distance = r1 + r2
+                    distance = np.sqrt((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2)            
+                    if distance < critical_distance:
+                        Good = False
+                        Saturated += 1
+                        break
+                if not Good:
+                    break
+            if not Good:
+                break        
+        if Good:
+            Molecules.append(Water2)
+            Saturated = 0
+    record = []
+    for j in Molecules:
+        for k in j.Atoms:
+            S = k.Atom.Symbol
+            x = str(k.x)
+            y = str(k.y)
+            z = str(k.z)
+            line = S + ': ' + x + '\t' + y + '\t' + z + '\n'
+            record.append(line)
+    record.append('\n')
+    for line in record:
+        records.append(line)
 
-
-
-
-
-
-
-
+f = open('Coord.x', "w")
+f.writelines(records)
+f.close()
